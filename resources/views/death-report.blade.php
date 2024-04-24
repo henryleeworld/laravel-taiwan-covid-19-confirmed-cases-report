f79381c5a74667e29fe0149c2c34b9f5d753fec9
Henry Lee <henryleeworld@gmail.com>
2024-04-25 00:18:20 +0800

    feat:更新最新資料

--- resources/views/death-report.blade.php
+++ resources/views/death-report.blade.php
@@ -7,29 +7,29 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" integrity="sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
-        <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.bootstrap5.min.css" />
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css" />
+        <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.bootstrap5.min.css" />
     </head>
     <body>
         <div class="container-fluid col-sm-12 mt-3">
             <table id="deaths-report" class="table table-striped cell-border compact" style="width: 100%;">
                 <thead>
                     <tr>
-                        <th>個案死亡日</th>
-                        <th>縣市別</th>
-                        <th>區域</th>
-                        <th>新增死亡人數</th>
-                        <th>累計死亡人數</th>
-                        <th>七天移動平均新增死亡人數</th>
+                        <th>{{ __('Case death date') }}</th>
+                        <th>{{ __('City') }}</th>
+                        <th>{{ __('Area') }}</th>
+                        <th>{{ __('New death toll') }}</th>
+                        <th>{{ __('Cumulative death toll') }}</th>
+                        <th>{{ __('Seven-day moving average of new deaths') }}</th>
                     </tr>
                 </thead>
             </table>
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js" integrity="sha512-VK2zcvntEufaimc+efOYi622VN5ZacdnufnmX7zIhCPmjhKnOi9ZDMtg1/ug5l183f19gG1/cBstPO4D8N/Img==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
         <script>
             $(function () {
                 $.extend(true, $.fn.dataTable.Buttons.defaults.dom.button, {
@@ -42,7 +42,7 @@
 
                 $("#deaths-report").DataTable({
                     language: {
-                        url: "https://cdn.datatables.net/plug-ins/1.13.4/i18n/zh-HANT.json",
+                        url: "https://cdn.datatables.net/plug-ins/1.13.5/i18n/zh-HANT.json",
                     },
                     buttons: dtButtons,
                     dom: 'lBfrtip<"actions">',
